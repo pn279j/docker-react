c2fbe756a663d986cebd01118b0aed2128a608aa
Priyanka Nagar <Priyanka@Priyankas-MacBook-Pro.local>
2019-05-25 12:33:42 -0700

    change app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there! dsadsakl;ewqke;qwlek;
+          I was changed on the Feature branch
         </a>
       </header>
     </div>
